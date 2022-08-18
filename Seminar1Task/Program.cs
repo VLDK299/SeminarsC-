3e0f35ea9b911d7cea48f744ea9d23909604ee5d
VL <VLDK299@yandex.ru>
2022-08-18 23:41:10 +0500

    Первое задание первого семинара

---
+++ Seminar1Task/Program.cs
@@ -0,0 +1,19 @@
+﻿int num1, num2; // Задаем переменные.
+
+Console.Write("Введите первое число: "); 
+
+num1 = Convert.ToInt32(Console.ReadLine()); // Вводим первое число.
+
+Console.Write("Введите второе число: ");
+
+num2 = Convert.ToInt32(Console.ReadLine()); // Вводим второе число.
+
+if (num1 == num2 * num2) // Задаем условие, чтобы проверить, является ли первое число квадратом второго.
+{
+    Console.WriteLine("Первое число равно квадрату второго");
+}
+else
+{
+    Console.WriteLine("Первое число не равно квадрату второго");
+}
+
