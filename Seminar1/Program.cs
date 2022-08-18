45a87931217a241e24eb622812241d4bc22d0f4e
VL <VLDK299@yandex.ru>
2022-08-18 23:40:28 +0500

    Первая часть семинара

---
+++ Seminar1/Program.cs
@@ -0,0 +1,19 @@
+﻿int num;
+
+// Ввод данных
+Console.Write("Введите число: ");
+
+num = Convert.ToInt32(Console.ReadLine());
+
+// Вычисление квадрата
+int sqare = num * num;
+
+if (sqare >= 100)
+{
+    Console.WriteLine("Such a big sqare: " + sqare);
+}
+else
+{
+    Console.WriteLine("Квадрат данного числа равен: " + sqare);
+}
+
