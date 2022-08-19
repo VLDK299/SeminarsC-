2dbb7beeb758db009733f32954289c88f6501667
VL <VLDK299@yandex.ru>
2022-08-19 17:32:16 +0500

    Seminar1Task2

---
+++ Seminar1Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿int day;
+Console.Write("Введите порядковый номер дня недели: ");
+day = Convert.ToInt32(Console.ReadLine());
+if (day == 1)
+{
+    Console.WriteLine("Понедельник"); // Продолжить дни недели
+}
+else if (day == 2)
+{
+    Console.WriteLine("Вторник");
+}
