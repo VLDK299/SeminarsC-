652d8eb9d62d7125521fc27f7d251b9c2fc051a4
VL <VLDK299@yandex.ru>
2022-09-29 18:08:07 +0500

    ShowArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,15 @@
 ﻿
 string[] array = new string[5] {"0", "11", "123", "4444", "555555"};
 
+void ShowArray(string[] array)
+{
+    Console.Write("[ ");
+    for(int i=0; i<array.Length; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.Write("]");
+}
+
+
+ShowArray(array);
